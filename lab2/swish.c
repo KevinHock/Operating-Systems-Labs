7f3d42e9357623d342fb9b4d4ea08ff47bcc7e68
CSE306 Student <cse306@vl109.cs.stonybrook.edu>
2013-02-17 20:27:00 -0500

    Trying to print PWD

--- lab2/swish.c
+++ lab2/swish.c
@@ -6,8 +6,35 @@
 // Assume no input line will be longer than 1024 bytes
 #define MAX_INPUT 1024
 
+int i=-1;
+int debug=1;//1 for development 0 for production
+
+void checkDebug(int argc, char** argv);
+void pwd(char **envp);
+
+// Go through and find PWD in **envp
+// Then print after = [4] to n
+void pwd(char **envp){
+	char** env;
+     	for(env = envp; *env != 0; env++){
+    		char* thisEnv = *env;
+		char[] ENV   = "ENV=";
+		char[3] thisEnvArr = *env;
+    		if(thisEnvArr==ENV){
+			printf("Woohoo\nWoohoo\n");
+		}  
+    	}
+}
+
+
+//If "cd" then "HOME"
+//If "cd -" then "OLDPWD"
+
 int main (int argc, char ** argv, char **envp) {
+	checkDebug(argc,argv);
+	printf("Debug = %d\n",debug);
 	int finished = 0;
+
 	char *prompt = "swish> ";
 	char cmd[MAX_INPUT];
 	
@@ -18,14 +45,23 @@ int main (int argc, char ** argv, char **envp) {
 		int rv;
     		int count;		
     		
+		// Print the current directory
+		// "[/tmp] "
+		// "To inspect your environment variables use the printenv command"
+		pwd(envp);
+		
    		// Print the prompt
+		// "swish> "
     		rv = write(1, prompt, strlen(prompt));
     		
+		
     		if (!rv) { 
       			finished = 1;
       			break;
     		}
-   		 
+   		
+		break;	
+		
     		// Read and parse the input
     		for(rv = 1, count = 0, cursor = cmd, last_char = 1;
 		rv && (++count < (MAX_INPUT-1))&& (last_char != '\n');
@@ -46,4 +82,13 @@ int main (int argc, char ** argv, char **envp) {
     		write(1, cmd, strnlen(cmd, MAX_INPUT));
   	}
   	return 0;
+}
+
+void checkDebug(int argc, char** argv){
+	for(i=1;i<argc;i++){
+    		if(argv[i][0]!='-')
+        		break;
+    		if(argv[i][1] == 'd')
+			debug=1;
+	}
 }
